527f932569cbccaf22eff9ca75c46ed1fc9b792d
mrquorr <sergio.93.op@gmail.com>
2016-02-21 00:18:47 -0600

    Added encryption and decryption to generateKeys.js

--- generateKeys.js
+++ generateKeys.js
@@ -1,5 +1,8 @@
 var sjcl = require('./core.js');
 
+// PUBLIC KEY GENERATION
+console.log("KEY GENERATION");
+
 crypto_keys = sjcl.ecc.elGamal.generateKeys(256);
 
 var public_key = crypto_keys.pub.get();
@@ -8,10 +11,20 @@ var secret_key = crypto_keys.sec.get();
 var public_key_hex = sjcl.codec.hex.fromBits(public_key.x) + sjcl.codec.hex.fromBits(public_key.y);
 var secret_key_hex = sjcl.codec.hex.fromBits(secret_key);
 
-console.log(public_key_hex + ":::" + secret_key_hex);
+  console.log(public_key_hex + ":::" + secret_key_hex);
+
+// SHA-256 HASHING
+console.log("HASHING");
 
 var bitArray = sjcl.hash.sha256.hash("message");
 var digest_sha256 = sjcl.codec.hex.fromBits(bitArray);
 
+  console.log(digest_sha256);
 
-console.log(digest_sha256);
+// ENCRYPTING AND DECRIPTING
+console.log("ENCRYPTION");
+
+var ct = sjcl.encrypt(crypto_keys.pub, "this is a secret message");
+//console.log(ct);
+var pt = sjcl.decrypt(crypto_keys.sec, ct);
+console.log(pt);
